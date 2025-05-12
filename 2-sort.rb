da9091a2ebca31a8de9024b028b7bdb5fc8e1fb8
OursBlanc42 <regniersimon@hotmail.com>
2025-05-12 05:40:00 +0200

    first commit

---
+++ 2-sort.rb
@@ -0,0 +1,18 @@
+###
+#
+#  Sort integer arguments (ascending) 
+#
+###
+
+result = []
+ARGV.each do |arg|
+    # skip if not integer
+    next if arg !~ /^-?[0-9]+$/
+
+    # convert to integer
+    i_arg = arg.to_i
+    result.insert(result.size,i_arg)
+    
+end
+
+puts result.sort
